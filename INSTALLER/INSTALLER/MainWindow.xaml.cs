ac3ff8750bf7c0603f056eb08c070731f871e86a
JOQUR <31622435+JOQUR@users.noreply.github.com>
2017-09-04 15:35:29 +0200

    Add files via upload
    pre-alpha albo jeszcze gorzej

---
+++ INSTALLER/INSTALLER/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace INSTALLER
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            ImageBrush myBrush = new ImageBrush();
+            myBrush.ImageSource =
+                new BitmapImage(new Uri(@"C:\Users\Kasia\source\repos\INSTALLER\image.png", UriKind.Absolute));
+            this.Background = myBrush;
+        }
+
+        private void IPdecbin_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window = new Window1();
+            window.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ INSTALLER/INSTALLER/Window1.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace INSTALLER
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        private void converting1()
+        {
+            int decf = Convert.ToInt32(_1.Text);
+
+
+
+
+            int remainder;
+            string result = string.Empty;
+            while (decf > 0)
+            {
+                remainder = decf % 2;
+                decf /= 2;
+                result = remainder.ToString() + result;
+            }
+
+            _11.Text = result;
+        }
+        private void converting2()
+        {
+            int decf = Convert.ToInt32(_2.Text);
+
+
+
+
+            int remainder;
+            string result = string.Empty;
+            while (decf > 0)
+            {
+                remainder = decf % 2;
+                decf /= 2;
+                result = remainder.ToString() + result;
+            }
+
+            _22.Text = result;
+        }
+        private void converting3()
+        {
+            int decf = Convert.ToInt32(_3.Text);
+
+
+
+
+            int remainder;
+            string result = string.Empty;
+            while (decf > 0)
+            {
+                remainder = decf % 2;
+                decf /= 2;
+                result = remainder.ToString() + result;
+            }
+
+            _33.Text = result;
+        }
+        private void converting4()
+        {
+            int decf = Convert.ToInt32(_4.Text);
+
+
+
+
+            int remainder;
+            string result = string.Empty;
+            while (decf > 0)
+            {
+                remainder = decf % 2;
+                decf /= 2;
+                result = remainder.ToString() + result;
+            }
+
+            _44.Text = result;
+        }
+
+        private void convert_Click(object sender, RoutedEventArgs e)
+        {
+
+            converting1();
+            converting2();
+            converting3();
+            converting4();
+        }
+    }
+}
